28a1606b76a23c967118095ad5465acda46c95f9
Mr-hotaka <101518075+Mr-hotaka@users.noreply.github.com>
2022-03-24 20:46:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,141 @@
-import tkinter as tk
-from tkinter import ttk
-class GreetingFrame(ttk.Frame):
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os, time
+from tkinter import CENTER
+
+def Admin_poduc():
+    while True:
+        try:
+            print("╔═════════════════════════════════╗")
+            print("║       Menu de productos:        ║")
+            print("║   1.Lista de productos          ║")
+            print("║   2.Dar de alta producto        ║")
+            print("║   4.Dar de baja de producto     ║")
+            print("║   3.Salir                       ║")
+            print("╚═════════════════════════════════╝")
+            opc_pro = int(input("Ingrese su opcion: "))
+            if opc_pro == 1:
+                print("Lista de productos: ")
+                input("Presiona Enter para continuar:")
+            elif opc_pro == 2:
+                dar_alta = input("Ingresa el producto:")
+                print()
+            elif opc_pro == 3:
+                dar_baja = input("Dar de baja producto")
+            elif opc_pro == 4:
+                break
+        except:
+            print("El dato no es valido.")
+
+def Admin_pers():
+    while True:
+        try:
+            print("╔═════════════════════════════════╗")
+            print("║       Menu de personal:         ║")
+            print("║   1.Lista de personal           ║")
+            print("║   2.Dar de alta personal        ║")
+            print("║   3.Dar de baja personal        ║")
+            print("║   4.Salir                       ║")
+            print("╚═════════════════════════════════╝")
+            opc_per = int(input("Ingrese su opcion: "))
+            if opc_per == 1:
+                print("Lista de personal: ")
+                input("Presiona Enter para continuar:")
+            elif opc_per == 2:
+                dar_alta_per= input("Dar de alta personal: ")
+                print()
+            elif opc_per == 3:
+                dar_baja_per = input("Dar de baja producto")
+            elif opc_per == 4:
+                break
+        except:
+            print()
+def Admin_credi():
+    while True:
+        try:
+            print("╔═════════════════════════════════╗")
+            print("║       Menu de credito:          ║")
+            print("║   1.Lista de credito            ║")
+            print("║   2.Ingresar credito            ║")
+            print("║   3.Pagar credito               ║")
+            print("║   4.Salir                       ║")
+            print("╚═════════════════════════════════╝")
+            opc_cre = int(input("Ingrese su opcion: "))
+            if opc_cre == 1:
+                print("Lista de creditos: ")
+                input("Presiona Enter para continuar:")
+            elif opc_cre == 2:
+                dar_nuevo_cred= input("Ingrese Credito: ")
+                print()
+            elif opc_cre == 3:
+                dar_pagar_cred = input("Pago de credito de cliente: ")
+            elif opc_cre == 4:
+                break
+        except:
+            print()
+car = "     ▄"
+car2 = "▄"
+Men = ["╔╗──╔╗──╔╗─────────────────────────────╔╗", "║╚╗╔╝║─╔╝╚╗────────────────────────────║║",
+    "╚╗║║╔╬═╩╗╔╬══╦═╗╔╗─╔╗╔══╦══╦══╦══╦╦══╦═╝╠══╦══╗","─║╚╝╠╣╔═╣║║╔╗║╔╝║║─║║║╔╗║══╣╔╗║╔═╬╣╔╗║╔╗║╔╗║══╣",
+    "─╚╗╔╣║╚═╣╚╣╚╝║║─║╚═╝║║╔╗╠══║╚╝║╚═╣║╔╗║╚╝║╚╝╠══║","──╚╝╚╩══╩═╩══╩╝─╚═╗╔╝╚╝╚╩══╩══╩══╩╩╝╚╩══╩══╩══╝",
+    "────────────────╔═╝║","────────────────╚══╝"]
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        self.name_entry = ttk.Entry(self)
-        self.name_entry.pack()
-        
-        self.greet_button = ttk.Button(
-            self, text="Saludar", command=self.say_hello)
-        self.greet_button.pack()
-        
-        self.greet_label = ttk.Label(self)
-        self.greet_label.pack()
+for i in range(8):
+    print(Men[i])
+time.sleep(10)
+for i in range(21):
+    os.system("cls")
+    print(car)
+    if i > 19:
+        print("       Carga completada")
+    elif i < 20:
+        print("           Cargando")
     
-    def say_hello(self):
-        self.greet_label["text"] = \
-            "¡Hola, {}!".format(self.name_entry.get())
-class AboutFrame(ttk.Frame):
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        self.label = ttk.Label(self)
-        self.label["text"] = ("Visitanos en recursospython.com y "
-                              "foro.recursospython.com.")
-        self.label.pack()
-        
-        self.web_button = ttk.Button(self, text="Visitar web")
-        self.web_button.pack(pady=10)
-        
-        self.forum_button = ttk.Button(self, text="Visitar foro")
-        self.forum_button.pack()
-class Application(ttk.Frame):
-    
-    def __init__(self, main_window):
-        super().__init__(main_window)
-        main_window.title("Panel de pestañas en Tcl/Tk")
-        
-        self.notebook = ttk.Notebook(self)
-        
-        self.greeting_frame = GreetingFrame(self.notebook)
-        self.notebook.add(
-            self.greeting_frame, text="Saludos", padding=10)
-        
-        self.about_frame = AboutFrame(self.notebook)
-        self.notebook.add(
-            self.about_frame, text="Acerca de", padding=10)
-        
-        self.notebook.pack(padx=10, pady=10)
-        self.pack()
-main_window = tk.Tk()
-app = Application(main_window)
-app.mainloop()
+    car = car + car2
+    time.sleep(1)
+
+input("\nPor favor presione la tecla Enter para continuar:")
+os.system("cls")
+
+while True:
+    try:
+        print("╔═════════════════════════════════╗")
+        print("║      Inicio de secion:          ║")
+        print("╚═════════════════════════════════╝")
+        val1 = input("Ingrese su cuanta:")
+        val2 = int(input("Ingresar su conraseña:"))
+        if (val1 == "admin" and val2 == 1234):
+            break
+        else:
+            os.system("cls")
+            print("El dato no es valido.")
+    except:
+        print("El dato no es correcto.")
+
+while True:
+    try:
+        print("╔═════════════════════════════════╗")
+        print("║             Menu:               ║")
+        print("║   1.Administrar productos       ║")
+        print("║   2.Administrar personal        ║")
+        print("║   3.Creditos                    ║")
+        print("║   4.Salir                       ║")
+        print("╚═════════════════════════════════╝")
+        opc_0 = int(input("Ingrese la opcion: "))
+        if opc_0  == 1:
+            print("Administrar productos")
+            Admin_poduc()
+        elif opc_0 == 2:
+            print("2.Administrar personal")
+            Admin_pers()
+        elif opc_0 == 3:
+            print("3.Administrar creditos")
+            Admin_credi()
+        elif opc_0 == 4:
+            break
+        else:
+            print("Error")
+    except:
+        print("El dato no es numérico")
